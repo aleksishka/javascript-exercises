9d9caa9d6d134af1def4a164f086b7fdb093b1ea
aleksishka <ashkabara95@gmail.com>
2023-12-20 16:12:51 +0100

    passed all the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let celsius = (far - 32) * (5 / 9); 
+  let round = Math.round(celsius * 10) / 10;
+  return round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahrenh = (cel * 9 / 5) + 32; 
+  let round = Math.round(fahrenh * 10) / 10;
+  return round;
+
 };
 
 // Do not edit below this line
